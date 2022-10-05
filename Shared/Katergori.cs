6268638631e8386247841146c10e74946cedfb5a
Mobiloka <Mobiloka@DESKTOP-0N2NNAG>
2022-10-05 21:27:48 +0700

    Add project files.

---
+++ Shared/Katergori.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ecommerce_mini.Shared
+{
+    internal class Katergori
+    {
+        public int KatergoriId { get; set; }
+        public string KatergoriName { get; set; }
+        public string KatergoriImg { get; set; }
+        public string KatergoriSlug { get; set; }
+        public int KatergoriStatus { get; set; }
+    }
+    internal class Produk
+    {
+        public int ProdukiId { get; set; }
+        public string ProdukName { get; set; }
+        public string ProdukImg { get; set; }
+        public string ProdukSlug { get; set; }
+        public int ProdukStatus { get; set; }
+        public int ProdukHarga { get; set; }
+        public int ProdukBintang { get; set; }
+    }
+
+    
+
+}
